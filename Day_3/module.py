842414399299d2933717aab5255727f5fdfa65af
pankaj gupta <pg925303@gmail.com>
2022-09-13 00:29:40 +0530

    current changes

--- Day_3/module.py
+++ Day_3/module.py
@@ -1,5 +1,9 @@
 number = int(input("Which number do you want to check ? "))
+
+
+
 if number % 2 == 0:
-    print(number," is a even number")
+    print(number, " is a even number")
 else:
-    print(number," is a odd number")
+    print(number, " is a odd number")
+
